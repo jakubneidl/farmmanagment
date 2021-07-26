b1283768aba6449827a2ac77caac6a4916b82f89
unknown <jakub.neidl@gmail.cz>
2021-07-26 21:06:06 +0200

    SpringBoot project init

---
+++ src/main/java/eu/biosedlak/farmmanagment/FarmmanagmentApplication.java
@@ -0,0 +1,13 @@
+package eu.biosedlak.farmmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmmanagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmmanagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/biosedlak/farmmanagment/FarmmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.biosedlak.farmmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmmanagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
